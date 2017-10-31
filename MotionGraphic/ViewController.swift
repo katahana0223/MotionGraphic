1515d6a69965f233ee540d07166d14df8711969c
hahahahahanananana <hkataoka925@st1.shinagawajoshi.ed.jp>
2017-10-31 20:38:08 +0900

    20171031

--- MotionGraphic/ViewController.swift
+++ MotionGraphic/ViewController.swift
@@ -7,19 +7,38 @@
 //
 
 import UIKit
+import C4
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: CanvasController {
+    
+    private var blueShape: Shape!
+    override func setup() {
+        self.canvas.backgroundColor = black
+        loadShape()
+       
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        blueShape.rotation = -0.5 * M_PI
     }
-
-
+    func loadShape(){
+        
+        blueShape = createShape(center: canvas.center, color: Color(0x00E5FF), radius: 30.0)
+        blueShape.strokeStart = 0.0
+        blueShape.strokeEnd = 0.001
+    }
+    func createShape(center: Point, color: Color, radius: Double) -> Shape {
+        let shape = Circle(center: center, radius: radius)
+        shape.lineWidth = 30.0
+        shape.strokeColor = color
+        shape.fillColor = clear
+        self.canvas.add(shape)
+        return shape
+    }
+    @IBAction func didTapCanvas(_ sender: Any){
+        let move = ViewAnimation(duration: 6.0){
+            self.blueShape.strokeEnd = random01()
+        }
+        move.animate()
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'MotionGraphic' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod ’C4’
+  # Pods for MotionGraphic
+
+  target 'MotionGraphicTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'MotionGraphicUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
