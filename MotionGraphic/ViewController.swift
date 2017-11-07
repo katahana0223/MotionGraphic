651c64d6d33747edd74027d7a45c865d4a1af0f9
hahahahahanananana <hkataoka925@st1.shinagawajoshi.ed.jp>
2017-11-07 18:34:47 +0900

    20171107

--- MotionGraphic/ViewController.swift
+++ MotionGraphic/ViewController.swift
@@ -18,24 +18,25 @@ class ViewController: CanvasController {
        
     }
     override func viewWillAppear(_ animated: Bool) {
+        //始まる方向を指定
         blueShape.rotation = -0.5 * M_PI
     }
     func loadShape(){
-        
-        blueShape = createShape(center: canvas.center, color: Color(0x00E5FF), radius: 30.0)
+        //radius円の大きさ
+        blueShape = createShape(center: canvas.center, color: Color(0x00E5FF), radius: 50.0)
         blueShape.strokeStart = 0.0
         blueShape.strokeEnd = 0.001
     }
     func createShape(center: Point, color: Color, radius: Double) -> Shape {
         let shape = Circle(center: center, radius: radius)
-        shape.lineWidth = 30.0
+        shape.lineWidth = 20.0
         shape.strokeColor = color
         shape.fillColor = clear
         self.canvas.add(shape)
         return shape
     }
     @IBAction func didTapCanvas(_ sender: Any){
-        let move = ViewAnimation(duration: 6.0){
+        let move = ViewAnimation(duration: 1.0){
             self.blueShape.strokeEnd = random01()
         }
         move.animate()
